2689f58781f4f64a1aa52da0e2cbbc6c8890ab90
A <alan61@centrum.cz>
2021-05-25 15:09:10 +0200

    Spike rotations

--- src/objects/Spike.java
+++ src/objects/Spike.java
@@ -4,15 +4,38 @@ import java.awt.*;
 
 public class Spike extends Polygon {
     public static int blockSize = Block.blockSize;
-    private static int thinness = blockSize/15;
+    private static final int thinness = blockSize/15;
 
     private final int width = blockSize;
     private final int height = blockSize;
 
-    public Spike(int x, int y) {
-        addPoint(x-blockSize+thinness, y);
-        addPoint(x-thinness, y);
-        addPoint(x-blockSize/2, y-blockSize);
+    public Spike(int x, int y, int rotation) {
+        switch (rotation) {
+            // Facing up
+            case 1 -> {
+                addPoint(x - blockSize + thinness, y); // Left point
+                addPoint(x - thinness, y); // Right point
+                addPoint(x - blockSize / 2, y - blockSize); // Middle point
+            }
+            // Facing down
+            case 2 -> {
+                addPoint(x - blockSize + thinness, y - blockSize);
+                addPoint(x - thinness, y - blockSize);
+                addPoint(x - blockSize / 2, y);
+            }
+            // Facing left
+            case 3 -> {
+                addPoint(y, x - blockSize + thinness);
+                addPoint(y, x - thinness);
+                addPoint(y - blockSize, x - blockSize / 2);
+            }
+            // Facing right
+            case 4 -> {
+                addPoint(y - blockSize, x - blockSize + thinness);
+                addPoint(y - blockSize, x - thinness);
+                addPoint(y, x - blockSize / 2);
+            }
+        }
     }
 
     public void draw(Graphics g) {
